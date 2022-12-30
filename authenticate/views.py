466afab3d2391b5f66dc7dce1176aac99cf0f26d
gozygreg <goziennabuife@yahoo.com>
2022-12-30 01:27:09 +0000

    Create view logic for logout

--- authenticate/views.py
+++ authenticate/views.py
@@ -21,3 +21,9 @@ def login_user(request):
             return redirect('login')
     else:
         return render(request, 'authenticate/login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logout'))
+    return redirect('home')
