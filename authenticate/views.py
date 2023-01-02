df5d11fbf229e8350a6396529a6f4c01b96775ad
gozygreg <goziennabuife@yahoo.com>
2023-01-02 04:02:41 +0000

    Use widget to customise form

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(request):
@@ -27,3 +29,20 @@ def logout_user(request):
     logout(request)
     messages.success(request, ('You have been logout'))
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('You Have Registered'))
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    context = {'form': form}
+    return render(request, 'authenticate/register.html', context)
